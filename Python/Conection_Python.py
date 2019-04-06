8e6297cb0b8adc23087fd2598c4416a4cfc113d0
Alejandro <alegamboaba.17@gmail.com>
2019-04-06 19:36:37 +0200

    add requirements and changes in Conection_Python

--- Python/Conection_Python.py
+++ Python/Conection_Python.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
+if __name__ == '__main__':
 
-def getdata():
-    api_key = '0466ad07eb8e68af9cd1923bf5fbf9cb85ab13c8'
-    guid = 'SALUD-99027'
-    url = 'http://api.datosabiertos.presidencia.go.cr/api/v2/dashboards/' + guid + '.json/?auth_key=' + api_key
-    response = requests.get(url) #use request to get data from url
+    api_key = 'baf0fa5633774e38fc5ce95b7945b884a8dd26dd'
+    guid = 'INVER-EN-ID-SEGUN-AREA'
+    url = 'http://micit.cloudapi.junar.com/api/v2/visualizations/' + guid + '.json/?auth_key=' + api_key
+    response = requests.get(url)  # use request to get data from url
     if response.status_code == 200:
         print(response.content)  # show data in json format
         data = json.loads(response.content)  # convert json format to python object
@@ -14,4 +14,3 @@ def getdata():
         print(data["user"])  # access to an element from dict by example user
 
 
-getdata()
